3dabdd51a1fcdb4e3c252bbbd846c6c16cb535a6
Tim Carlson <tim.carlson@velocloud.net>
2017-06-24 12:49:55 -0700

    refactor( RE stats ): Added newline between iostat and top output
    Split up the output from `iostat` and `top` for slightly more readable stats

--- stats.sh
+++ stats.sh
@@ -6,6 +6,7 @@ git --git-dir=$DIR/.git --work-tree=$DIR checkout $DIR/stats.txt
 git --git-dir=$DIR/.git --work-tree=$DIR pull
 
 iostat > $DIR/stats.txt
+printf "\n" >> $DIR/stats.txt
 top -l 1 -ncols 7 -o mem -stats command,mem,cpu,threads -n 5 >> $DIR/stats.txt
 
 git --git-dir=$DIR/.git --work-tree=$DIR add $DIR/stats.txt
